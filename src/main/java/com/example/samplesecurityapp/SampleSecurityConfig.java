e100289bd1b81cc97425c92eb979a86b8022abc4
yuta6999 <48405817+yuta6999@users.noreply.github.com>
2024-08-29 22:33:59 +0900

    ▼リスト7-38　ログインページ（セキュリティ）

--- src/main/java/com/example/samplesecurityapp/SampleSecurityConfig.java
+++ src/main/java/com/example/samplesecurityapp/SampleSecurityConfig.java
@@ -1,4 +1,4 @@
-//▼リスト7-23
+//▼リスト7-34
 package com.example.samplesecurityapp;
 
 import javax.sql.DataSource;
@@ -6,75 +6,35 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
 public class SampleSecurityConfig {
+  @Autowired
+  private DataSource dataSource;
   
-  //  ▼リスト7-32
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http)
       throws Exception {
     http.csrf().disable();
     http.authorizeHttpRequests(authorize -> {
-      authorize
-        .requestMatchers("/").permitAll()
-        .requestMatchers("/js/**").permitAll()
-        .requestMatchers("/css/**").permitAll()
-        .requestMatchers("/img/**").permitAll()
-        .requestMatchers("/admin").hasRole("ADMIN")
-        .anyRequest().authenticated();
+      authorize.anyRequest().permitAll();
     });
+//    ▼リスト7-36
     http.formLogin(form -> {
-      form.defaultSuccessUrl("/secret");
+      form.defaultSuccessUrl("/secret")
+        .loginPage("/login");
     });
     return http.build();
   }
-  
-	//  ▼リスト7-30
-	@Autowired
-	private DataSource dataSource;
-	
-	@Bean
-	public UserDetailsManager userDetailsManager(){
-	 JdbcUserDetailsManager users = new JdbcUserDetailsManager(this.dataSource);
-	 
-	 // ユーザー登録（※一度だけ実行する）
-     //users.createUser(makeUser("taro","yamada", "USER"));
-	 //users.createUser(makeUser("hanako","flower", "USER"));
-	 //users.createUser(makeUser("sachiko","happy", "USER"));
-	 //	▼リスト7-31
-	 //users.createUser(makeUser("admin","kanri", "ADMIN"));
 
-	 return users;
-	}
-	
-	private UserDetails makeUser(String user, String pass, String role) {
-	 return User.withUsername(user)
-	 .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(pass))
-	   .roles(role)
-	   .build();
-	}
-//  @Bean
-//  public InMemoryUserDetailsManager userDetailsManager(){
-//    String username = "user";
-//    String password = "pass";
-//
-//    UserDetails user = User.withUsername(username)
-//      .password(
-//        PasswordEncoderFactories
-//          .createDelegatingPasswordEncoder()
-//          .encode(password))
-//      .roles("USER")
-//      .build();
-//    return new InMemoryUserDetailsManager(user);
-//  }
+  @Bean
+  public UserDetailsManager userDetailsManager(){
+    return new JdbcUserDetailsManager(this.dataSource);
+  }
 }
--- src/main/java/com/example/samplesecurityapp/SampleSecurityController.java
+++ src/main/java/com/example/samplesecurityapp/SampleSecurityController.java
@@ -1,16 +1,17 @@
-//▼リスト7-24
+//▼リスト7-35
 package com.example.samplesecurityapp;
 
-import jakarta.servlet.http.HttpServletRequest;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SampleSecurityController {
   
   @RequestMapping("/")
+  @PreAuthorize("permitAll")
   public ModelAndView index(ModelAndView mav) {
     mav.setViewName("index");
     mav.addObject("title", "Index page");
@@ -19,21 +20,36 @@ public class SampleSecurityController {
   }
 
   @RequestMapping("/secret")
-  public ModelAndView secret(ModelAndView mav, HttpServletRequest request) {
-    String user = request.getRemoteUser();
-    String msg = "This is secret page. [login by \"" + user + "\"]";
+  @PreAuthorize("isAuthenticated()")
+  public ModelAndView secret(ModelAndView mav) {
     mav.setViewName("Secret");
     mav.addObject("title", "Secret page");
-    mav.addObject("msg", msg);
+    mav.addObject("msg", "This is secret page.");
     return mav;
   }
-  
-//  ▼リスト7-33
+
   @RequestMapping("/admin")
+  @PreAuthorize("hasRole('ADMIN')")
   public ModelAndView admin(ModelAndView mav) {
     mav.setViewName("index");
     mav.addObject("title", "Admin page");
     mav.addObject("msg", "This is only access ADMIN!");
     return mav;
   }
+  
+//  ▼リスト7-38
+  @RequestMapping("/login")
+  @PreAuthorize("permitAll")
+  public ModelAndView login(ModelAndView mav,
+      @RequestParam(value="error", required=false)String error) {
+    mav.setViewName("login");
+    System.out.println(error);
+    if (error != null) {
+      mav.addObject("msg", "ログインできませんでした。");
+    } else {
+      mav.addObject("msg", "ユーザー名とパスワードを入力：");
+    }
+    return mav;
+  }
+
 }
